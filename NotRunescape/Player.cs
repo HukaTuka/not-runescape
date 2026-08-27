1d270bb3d002edf123b709512c29ea5f3757df4f
Kennie Ankersø <kan@easv.dk>
2026-08-27 10:01:36 +0200

    feature: add player support with inventory

---
+++ NotRunescape/Player.cs
@@ -0,0 +1,30 @@
+﻿namespace NotRunescape;
+
+public class Player
+{
+    public int CurrentHp { get; set; } = 35;
+    public int MaxHp { get; set; } = 35;
+    public int Gold { get; set; } = 50;
+    public Dictionary<string, int> Inventory { get; set; } = new()
+    {
+        { "Lobster", 3 },
+        { "Rune Scimitar", 1 }
+    };
+
+    public void AddItem(string item, int amount)
+    {
+        if (Inventory.ContainsKey(item))
+            Inventory[item] += amount;
+        else
+            Inventory[item] = amount;
+    }
+
+    public void PrintInventory()
+    {
+        Console.WriteLine("\n--- Inventory ---");
+        foreach (var item in Inventory)
+        {
+            Console.WriteLine($"- {item.Key}: {item.Value}");
+        }
+    }
+}
